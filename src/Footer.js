5a6df29ef2a4b5c84a77bdcc41a84c38bc2efc60
Ryan Bagan <darcknight1980@gmail.com>
2023-02-28 02:47:12 -0800

    Added something, I don't know

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render () {
+    return(
+      <footer>&copy Ryan Bagan 2023</footer>
+    )
+  }
+
+
+}
+
+export default Footer;

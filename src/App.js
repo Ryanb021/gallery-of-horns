19ea83f46e6707c7ad7a16dc383b79c5c5c0238a
Ryan Bagan <darcknight1980@gmail.com>
2023-03-01 13:00:38 -0800

    made changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,51 @@ import Header from './Header'
 import Footer from './Footer';
 import data from './data.json';
 import './App.css';
+//import { Modal } from 'bootstrap';
+import Modal from 'react-bootstrap/Modal'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hearts: '',
+        showModal: false,
+        personName: ''
+    }
+  }
 
+  addHearts = () => {
+    this.setState({
+      hearts: this.state.hearts + '💖'
+    })
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  handleOpenModal = (name) => {
+    this.setState({
+      showModal: true,
+      personName: name
+    })
+  }
   render() {
     return (
       <>
-        <Header />
-        <Main data={data}/>
+        <Header hearts={this.state.hearts} />
+        <Main 
+        data={data} 
+        addHearts={this.addHearts}
+        handleOpenModal={this.handleOpenModal} />
         <Footer />
+        <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.state.personName}</Modal.Title>
+          </Modal.Header>
+        </Modal>
       </>
     );
   }

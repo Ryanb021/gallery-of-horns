476add180556a8dfbe1b8ff253bbbcd138888081
Ryan Bagan <darcknight1980@gmail.com>
2023-03-03 00:25:33 -0800

    finalized

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import './Form.css';
 
 class FormComponent extends React.Component {
 
@@ -9,17 +10,19 @@ class FormComponent extends React.Component {
       <>
         <Form onSubmit={this.props.hornCount}>
           <Form.Group controlId="filterHorns">
-            <Form.Label>
+            <Form.Label className="hornMan" style={{ width: "100%" }}>
               Select Your Amount of Horns
+              <Form.Select className="selection" name="select" onChange={this.props.select}>
+                <option value="all">All Horns</option>
+                <option value="1">1 Horn</option>
+                <option value="2">2 Horns</option>
+                <option value="3">3 Horns</option>
+                <option value="4">Too Much Horns Going on Here</option>
+              </Form.Select>
+              <Button className="press" type="submit">Get Horny</Button>
             </Form.Label>
-            <Form.Select name="select" onChange={this.props.select}>
-              <option value="all">All Horns</option>
-              <option value="1">1 Horn</option>
-              <option value="2">2 Horns</option>
-              <option value="3">3 Horns</option>
-              <option value="4">Too Much Horns Going on Here</option>
-            </Form.Select>
-            <Button type="submit">Get Horny</Button>
+
+
           </Form.Group>
         </Form>
       </>
